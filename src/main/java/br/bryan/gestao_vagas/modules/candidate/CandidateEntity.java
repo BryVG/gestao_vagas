1152aad9e1f3e1f4288c99c92bd11de74c2f3325
Bryan Vinicius Gomes <gomesbryan754@gmail.com>
2026-08-25 07:43:27 -0300

    Initialize project structure with Maven, including application properties, main application class, and candidate module with controller and entity.

---
+++ src/main/java/br/bryan/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,31 @@
+package br.bryan.gestao_vagas.modules.candidate;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+
+@Getter
+@Setter
+@Data
+public class CandidateEntity {
+    
+    private UUID id;
+    private String name;
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo username não deve conter espaços")
+    private String username;
+
+    @Email(message = "O campo (email) deve conter um e-mail válido")
+    private String email;
+
+    @Length(min = 10, max = 20)
+    private String password;
+    private String description;
+    private String Curriculum;
+}
---
+++ src/main/java/br/bryan/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,22 @@
+package br.bryan.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.bryan.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/candidates")
+public class CandidateController {
+
+    @PostMapping("/")
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        // Lógica para criar um candidato
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
+    }
+    
+}
