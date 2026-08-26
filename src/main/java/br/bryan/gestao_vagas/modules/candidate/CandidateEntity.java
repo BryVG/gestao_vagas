77f81bb48a4393dfdce8fe522d84839c2249eab6
Bryan Vinicius Gomes <gomesbryan754@gmail.com>
2026-08-26 07:46:03 -0300

    Api Gestao de vagas

--- src/main/java/br/bryan/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/bryan/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -5,19 +5,29 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.UUID;
+import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+
 
 @Getter
 @Setter
 @Data
 public class CandidateEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
+
+    @NotBlank
     @Pattern(regexp = "^(?!\\s*$).+", message = "O campo username não deve conter espaços")
     private String username;
 
@@ -28,4 +38,7 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String Curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
